a9a12e9f0ca99d9e3939cfe98f65d9cedab08351
fshanom <35174505+fshanom@users.noreply.github.com>
2022-05-17 00:08:04 -0300

    Upload trabalho semáforos

---
+++ trab-1-semaforo/Teste.java
@@ -0,0 +1,29 @@
+import pc.Consumidor;
+import pc.Numero;
+import pc.Produtor;
+import pc.Semaforo;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		
+		int numeroPermissoes = 1;
+		Semaforo mutex = new Semaforo(numeroPermissoes);
+		Semaforo cheio = new Semaforo(0);
+		Semaforo vazio = new Semaforo(1);
+		
+		Numero n = new Numero(0, mutex, cheio, vazio);
+		Produtor p = new Produtor(n);
+		Consumidor c = new Consumidor(n);
+		Consumidor c1 = new Consumidor(n);
+		Consumidor c2 = new Consumidor(n);
+		
+				
+		p.start();
+		c.start();
+		c1.start();
+		c2.start();
+
+	}
+
+}
---
+++ trab-1-semaforo/pc/Numero.java
@@ -0,0 +1,31 @@
+package pc;
+
+public class Numero {
+	private int numero;
+	private Semaforo mutex, cheio, vazio;
+
+	public Numero(int numero, Semaforo mutex, Semaforo cheio, Semaforo vazio) {
+		this.numero = numero;
+		this.mutex = mutex;
+		this.cheio = cheio;
+		this.vazio = vazio;
+	}
+
+	public int consumir(String idThread) {
+		cheio.P();
+		mutex.P();
+		System.out.println("  Consumidor " + idThread + "\t consumiu: \t" + this.numero);
+		mutex.V();
+		vazio.V();
+		return this.numero;
+	}
+
+	public void produzir(String idThread, int valor) {
+		vazio.P();
+		mutex.P();
+		this.numero = valor;
+		System.out.println("Produtor " + idThread + "\t produziu: \t" + valor);
+		mutex.V();
+		cheio.V();
+	}
+}
